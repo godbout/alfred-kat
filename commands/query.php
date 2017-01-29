0fe08fefd9d3a6cbc65a6d6f8a03b22aa570f1e9
Guillaume Leclerc <guill.bout@gmail.com>
2017-01-29 23:35:58 +0800

    Basic refactoring

--- commands/query.php
+++ commands/query.php
@@ -20,12 +20,14 @@ $dom = new IvoPetkov\HTML5DOMDocument();
 $dom->loadHTML($response);
 $rows = $dom->querySelector('table.data')->querySelectorAll('tr.odd');
 foreach ($rows as $row) {
-    preg_match_all('/class="cellMainLink">(.*?)<\/a>/s', $row->innerHTML, $title);
-    preg_match_all('/class="center" title="(.*?)">(.*?)<\/td>/s', $row->innerHTML, $date);
-    preg_match_all('/<td class="nobr center">(.*?)<\/td>/s', $row->innerHTML, $size);
-    preg_match_all('/class="green center">(.*?)<\/td>/s', $row->innerHTML, $seeders);
-    preg_match_all('/class="red lasttd center">(.*)<\/td>/s', $row->innerHTML, $leechers);
-    preg_match_all('/<a data-nop title="Torrent magnet link" href="(.*?)"/s', $row->innerHTML, $magnet);
+    $html = $row->innerHTML;
+
+    preg_match_all('/class="cellMainLink">(.*?)<\/a>/s', $html, $title);
+    preg_match_all('/class="center" title="(.*?)">(.*?)<\/td>/s', $html, $date);
+    preg_match_all('/<td class="nobr center">(.*?)<\/td>/s', $html, $size);
+    preg_match_all('/class="green center">(.*?)<\/td>/s', $html, $seeders);
+    preg_match_all('/class="red lasttd center">(.*)<\/td>/s', $html, $leechers);
+    preg_match_all('/<a data-nop title="Torrent magnet link" href="(.*?)"/s', $html, $magnet);
 
     $subtitle = trim(html_entity_decode($date[1][0])) . ', ' . trim(html_entity_decode($size[1][0])) . ', seeders: ' . trim($seeders[1][0]) . ', leechers: ' . trim($leechers[1][0]);
 
