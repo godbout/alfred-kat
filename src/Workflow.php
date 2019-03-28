8902f64063fe22e57eb9de08e55fbdedd9dc2c0d
Guillaume Leclerc <guill.bout@gmail.com>
2019-03-28 18:29:43 +0800

    wip

--- src/Workflow.php
+++ src/Workflow.php
@@ -2,10 +2,62 @@
 
 namespace Godbout\Alfred\Kat;
 
+use Goutte\Client;
+use Godbout\Alfred\Workflow\Icon;
+use Godbout\Alfred\Workflow\Item;
+use Godbout\Alfred\Workflow\ScriptFilter;
+
 class Workflow
 {
     public static function resultsFor($term = '')
     {
-        return 'Fight Club (1999)';
+        $client = new Client();
+
+        $crawler = $client->request('GET', getenv('url') . urlencode($term));
+
+        $crawler->filter('.firstr')->nextAll()->each(function ($row) {
+            ScriptFilter::add(
+                Item::create()
+                    ->title(trim(self::itemTitle($row)))
+                    ->subtitle(trim($row->text()))
+                    ->icon(Icon::create("resources/icons/magnet.png"))
+            );
+        });
+
+        return ScriptFilter::output();
+    }
+
+    public static function itemTitle($row)
+    {
+        $itemMetada = [];
+
+        $row->children('td')->nextAll()->each(function ($column) use (&$itemMetada) {
+            $itemMetada[] = trim($column->text());
+        });
+
+        return self::buildItemTitle($itemMetada);
+    }
+
+    public static function buildItemTitle($metadata)
+    {
+        [$timeagoNumericPart, $timeagoAlphaPart] = self::buildTimeagoValue($metadata[2]);
+
+        return "$timeagoNumericPart $timeagoAlphaPart ago by $metadata[1], $metadata[0], $metadata[3] seeders ($metadata[4] l)";
+    }
+
+    public static function buildTimeagoValue($timeago)
+    {
+        $numbericPart = intval($timeago);
+
+        $alphaPart = str_replace($numbericPart, '', $timeago);
+
+        return [$numbericPart, $alphaPart];
+    }
+
+    public static function userInput()
+    {
+        global $argv;
+
+        return trim($argv[1] ?? '');
     }
 }
--- tests/WorkflowTest.php
+++ tests/WorkflowTest.php
@@ -2,14 +2,16 @@
 
 namespace Tests;
 
-use Godbout\Alfred\Kat\Workflow;
 use PHPUnit\Framework\TestCase;
+use Godbout\Alfred\Kat\Workflow;
 
 class WorkflowTest extends TestCase
 {
     /** @test */
     public function it_can_search_for_torrents_on_the_kat_site()
     {
+        putenv('url=https://kickass.sx/torrent/usearch/');
+
         $this->assertStringContainsString(
             'Fight Club (1999)',
             Workflow::resultsFor('fight club')
