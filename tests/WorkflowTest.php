6f3d85685635c19f6cce6ccc956bfc08c3f757a2
Guill Lo <guill.bout@gmail.com>
2019-06-08 23:52:27 +0800

    Add notify copy test and back copy test to null

--- tests/WorkflowTest.php
+++ tests/WorkflowTest.php
@@ -44,17 +44,28 @@ class WorkflowTest extends TestCase
      /** @test */
     public function it_can_copy_the_magnet_link_of_a_chosen_torrent()
     {
+        $this->markTestSkipped('This test needs to run under an osx environment');
+
         $this->assertTrue(
             Workflow::copy('/fight-club-1999-1080p-brrip-x264-yify-t446902.html')
         );
     }
 
     /** @test */
-    public function it_can_notify_the_user()
+    public function it_can_notify_the_user_when_download()
     {
         $this->assertStringContainsString(
             'Fight Club (1999) 1080p BrRip x264 - YIFY',
             Workflow::notifyDownload('Fight Club (1999) 1080p BrRip x264 - YIFY')
+        );
+    }
+
+    /** @test */
+    public function it_can_notify_the_user_when_copy()
+    {
+        $this->assertStringContainsString(
+            'Fight Club (1999)',
+            Workflow::notifyCopy('Fight Club (1999) 1080p BrRip x264 - YIFY')
         );
     }
 }
